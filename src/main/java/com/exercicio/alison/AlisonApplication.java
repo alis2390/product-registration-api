fca62430431ca27186d1fc13ce56558c1e15d9f6
Jackson Rodrigues <jacksonrodrigues@fitec.org.br>
2023-11-02 15:22:27 -0300

    fix: refactor a structure

---
+++ src/main/java/com/exercicio/alison/AlisonApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.alison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/alison/AlisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.alison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
